13d014b773bb4879555a24932fee68c344a36dce
vinh <sabervinh0306@gmail.com>
2020-07-06 13:22:54 +0700

    chinh sua js , them anh ,tao slide

--- js/javascript.js
+++ js/javascript.js
@@ -1,40 +1,45 @@
-// var array = document.querySelectorAll('#sync1 .Wrapper__showGroup')
-// for(var i = 0; i < array.length; i++){
-//     array[i].style.width = '1226px' 
-// }
-// document.querySelector('.Wrapper__showGroup').style.width = 1226*array.length+'px'
-// var widthcarousel = document.querySelector('.Wrapper__showGroup').offsetWidth
-// var carousel = document.querySelector('.Wrapper__showGroup')
-// var style = window.getComputedStyle(carousel);
-// var matrix = new WebKitCSSMatrix(style.webkitTransform)
-// function clickable(){
-//     document.querySelector('slide__btn').classList.add('active')
-// }
-// function clickdisable(){
-//     document.querySelector("slide__btn").classList.remove('active')
-// }
-// document.querySelector('#sildeshow__btn-next').addEventListener('click',next)
-// function next(){
-//     matrix.m41-=1226 
-//     if((-matrix.m41)>=widthcarousel){
-//         matrix.m41 = 0
-//         document.querySelector('.Wrapper__showGroup').style.transition = "all 800ms ease 0s"
-//         document.querySelector('.Wrapper__showGroup').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
-//     }else{
-//         document.querySelector('.Wrapper__showGroup').style.transition = "all 500ms ease 0s"
-// 		document.querySelector('.Wrapper__showGroup').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
-//     }
-// }
-// document.querySelector('#sildeshow__btn-next').addEventListener('click',prev)
-// function prev(){
-// 	if ((matrix.m41)==0) {
-// 		matrix.m41 = -widthcarousel +1226
-// 		document.querySelector('.Wrapper__showGroup').style.transition = "all 800ms ease 0s"
-// 		document.querySelector('.Wrapper__showGroup').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
-// 	}else{
-// 		matrix.m41+=790
-// 		document.querySelector('.Wrapper__showGroup').style.transition = "all 500ms ease 0s"
-// 		document.querySelector('.Wrapper__showGroup').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
-// 	}
-// }
-// var auto = setInterval(next,5000)
+
+
+
+var array = document.querySelectorAll('.Wrapper__item')
+for(var i = 0; i < array.length; i++){
+    array[i].style.width = '194px' 
+}
+document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.width = 5796+'px'//194*array.length
+var widthcarousel = document.querySelector('.slide__carousel__WrapperOut__Wrapper').offsetWidth
+var carousel = document.querySelector('.slide__carousel__WrapperOut__Wrapper')
+var style = window.getComputedStyle(carousel);
+var matrix = new WebKitCSSMatrix(style.webkitTransform)
+function clickable(){
+    document.querySelector('.control__buttons') .classList.add('.active')
+}
+function clickdisable(){
+    document.querySelector(".control__buttons") .classList.remove('.active')
+}
+document.querySelector('#control__button--next').addEventListener('click',next)
+function next(){
+    matrix.m41-=194*5+320 
+    if((-matrix.m41)>=3492)//widthcarousel
+    {
+        matrix.m41 -= -3354;
+        document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transition = "all 800ms ease 0s"
+        document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
+        
+    }else{
+        document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transition = "all 500ms ease 0s"
+		document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
+    }
+}
+document.querySelector('#control__button--prev').addEventListener('click',prev)
+function prev(){
+	if ((matrix.m41)==0) {
+		matrix.m41 = -3492
+		document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transition = "all 800ms ease 0s"
+		document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
+	}else{
+		matrix.m41+=194*5
+		document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transition = "all 500ms ease 0s"
+		document.querySelector('.slide__carousel__WrapperOut__Wrapper').style.transform = 'translate3d('+matrix.m41+'px, 0, 0)'
+	}
+}
+var auto = setInterval(next,10000)
